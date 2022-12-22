ecb55faa0a952f87179d52211c2f24cecfb24b5e
Surinder Singh Matoo <surinder83singh@gmail.com>
2022-12-23 04:02:48 +0530

    TrayIconBuilder -> TrayMenuBuilder

--- src/prelude.rs
+++ src/prelude.rs
@@ -3,6 +3,6 @@
 //! 
 pub use crate::application::Application;
 pub use crate::menu::{MenuItemBuilder, menu_separator, MenubarBuilder};
-pub use crate::tray::TrayIconBuilder;
+pub use crate::tray::TrayMenuBuilder;
 pub use crate::shortcut::ShortcutBuilder;
 pub use crate::media::VideoConstraints;
